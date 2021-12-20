c3a47823ccffc596a32a41f6ba00734a9c3d7614
Hernan Pedrino <hernanpedrino@gmail.com>
2021-12-20 14:01:13 -0300

    trabajo de listado del check out listo

--- src/app/pages/check-out/check-out.component.ts
+++ src/app/pages/check-out/check-out.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import Swal from 'sweetalert2';
+import { Pedido } from '../../models/pedido.model';
 
 @Component({
   selector: 'app-check-out',
@@ -8,8 +11,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CheckOutComponent implements OnInit {
 
-  constructor() { }
+  public itemsCarrito:Pedido[] = [];
+  public totalPedido:number = 0;
+  constructor(private router: Router) { }
 
-  ngOnInit(): void { }
+  ngOnInit(): void {
+    this.obtenerPedido();
+   }
+  obtenerPedido(){
+    const itmLs = localStorage.getItem('pedido');
+    if (itmLs) {
+      const arregItems = JSON.parse(itmLs);
+      arregItems.forEach((item:Pedido) => {
+        const total = item.precio * item.cantidad;
+        this.totalPedido += total;
+        this.itemsCarrito.push(item);
+      });
+    }
+  }
+  pagar(){
+    Swal.fire({
+      title: 'Muchas gracias por su compra',
+      icon: 'success'
+    })
+    localStorage.removeItem('pedido');
+    this.router.navigateByUrl('/home');
+  }
 
 }
